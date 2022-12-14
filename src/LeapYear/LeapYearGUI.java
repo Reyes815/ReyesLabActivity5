86e0bc94f3f9178123bd5cb9a33e650e31ac384c
Reyes815 <reyes.rowen30@gmail.com>
2022-12-14 14:13:38 +0800

    Rename Leap Year components

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,9 +1,21 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LeapYearGUI {
-    private JPanel panel1;
-    private JTextField textField1;
-    private JButton checkButton;
+    private JPanel LeapYearChecker;
+    private JTextField tfYear;
+    private JButton btnCheckYear;
+
+
+    public LeapYearGUI() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Hello");
+            }
+        });
+    }
 }
