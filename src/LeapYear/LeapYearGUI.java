978b9ce9119a37fafb9680982b966c2616cd5aa9
Reyes815 <reyes.rowen30@gmail.com>
2022-12-14 23:02:44 +0800

    Added the frame and buttons

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -4,18 +4,27 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class LeapYearGUI {
+public class LeapYearGUI extends JFrame{
     private JPanel LeapYearChecker;
     private JTextField tfYear;
     private JButton btnCheckYear;
+    private JPanel LeapYear;
 
+    public static void main(String[] args) {
+        LeapYearGUI frame = new LeapYearGUI();
+        frame.setContentPane(frame.LeapYear);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setSize(450,300);
+        frame.setVisible(true);
+    }
 
     public LeapYearGUI() {
-        btnCheckYear.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+       setTitle("Leap Year Checker");
+       btnCheckYear.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
                 JOptionPane.showMessageDialog(null, "Hello");
             }
-        });
+       });
     }
 }
