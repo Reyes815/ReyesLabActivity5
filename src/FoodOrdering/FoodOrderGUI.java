fc1fdb32ac98c697646b2e9e7cb44ac46ebe08ae
Reyes815 <reyes.rowen30@gmail.com>
2022-12-16 09:07:30 +0800

    grouped the radio buttons

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,10 +1,37 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FoodOrderGUI {
+public class FoodOrderGUI extends JFrame {
     private JPanel panel1;
+    private JButton orderButton;
     private JCheckBox pizzaCheckBox;
     private JRadioButton noneRadioButton;
-    private JButton orderButton;
+    private JRadioButton a5OffRadioButton;
+    private JRadioButton a10OffRadioButton;
+    private JRadioButton a15OffRadioButton;
+    orderHandler handler = new orderHandler();
+
+    public static void main(String[] args) {
+        FoodOrderGUI frame = new FoodOrderGUI();
+        frame.setContentPane(frame.panel1);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
+
+    public FoodOrderGUI() {
+        setTitle("Food Ordering System");
+        orderButton.addActionListener(handler);
+    }
+
+    public class orderHandler implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(null,"The total price is Php ");
+        }
+    }
+
 }
