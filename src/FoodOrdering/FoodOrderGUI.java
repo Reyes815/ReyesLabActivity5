1cbd3814b41875bbe86b5428a01fb4df50e80abc
Reyes815 <reyes.rowen30@gmail.com>
2022-12-16 09:17:43 +0800

    named all components for the Food Order GUI

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class FoodOrderGUI extends JFrame {
+public class FoodOrderGUI extends JFrame implements ActionListener{
     private JPanel panel1;
     private JButton orderButton;
     private JCheckBox pizzaCheckBox;
@@ -12,7 +12,14 @@ public class FoodOrderGUI extends JFrame {
     private JRadioButton a5OffRadioButton;
     private JRadioButton a10OffRadioButton;
     private JRadioButton a15OffRadioButton;
+    private JCheckBox burgerCheckBox;
+    private JCheckBox friesCheckBox;
+    private JCheckBox softDrinksCheckBox;
+    private JCheckBox teaCheckBox;
+    private JCheckBox sundaeCheckBox;
     orderHandler handler = new orderHandler();
+    double disc = 0;
+    double total = 0;
 
     public static void main(String[] args) {
         FoodOrderGUI frame = new FoodOrderGUI();
@@ -25,13 +32,32 @@ public class FoodOrderGUI extends JFrame {
     public FoodOrderGUI() {
         setTitle("Food Ordering System");
         orderButton.addActionListener(handler);
+        noneRadioButton.addActionListener(this);
+        a5OffRadioButton.addActionListener(this);
+        a10OffRadioButton.addActionListener(this);
+        a15OffRadioButton.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==noneRadioButton) {
+            disc = 1;
+        } else if (e.getSource()==a5OffRadioButton) {
+            disc = 0.05;
+        } else if (e.getSource()==a10OffRadioButton) {
+            disc = 0.10;
+        } else if (e.getSource()==a15OffRadioButton) {
+            disc = 0.15;
+        }
     }
 
     public class orderHandler implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            JOptionPane.showMessageDialog(null,"The total price is Php ");
+            JOptionPane.showMessageDialog(null,"The total price is Php " + disc);
         }
     }
+
+
 
 }
