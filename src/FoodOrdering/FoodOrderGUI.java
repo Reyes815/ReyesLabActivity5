39f80e8ba29443f8a0c904ca1900b53dabb02655
Reyes815 <reyes.rowen30@gmail.com>
2022-12-16 10:05:05 +0800

    renamed the components and added all the buttons and logic

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -7,16 +7,16 @@ import java.awt.event.ActionListener;
 public class FoodOrderGUI extends JFrame implements ActionListener{
     private JPanel panel1;
     private JButton orderButton;
-    private JCheckBox pizzaCheckBox;
-    private JRadioButton noneRadioButton;
-    private JRadioButton a5OffRadioButton;
-    private JRadioButton a10OffRadioButton;
-    private JRadioButton a15OffRadioButton;
-    private JCheckBox burgerCheckBox;
-    private JCheckBox friesCheckBox;
-    private JCheckBox softDrinksCheckBox;
-    private JCheckBox teaCheckBox;
-    private JCheckBox sundaeCheckBox;
+    private JRadioButton rbNone;
+    private JRadioButton rb5;
+    private JRadioButton rb10;
+    private JRadioButton rb15;
+    private JCheckBox cBurger;
+    private JCheckBox cPizza;
+    private JCheckBox cFries;
+    private JCheckBox cSoftDrinks;
+    private JCheckBox cTea;
+    private JCheckBox cSundae;
     orderHandler handler = new orderHandler();
     double disc = 0;
     double total = 0;
@@ -32,29 +32,50 @@ public class FoodOrderGUI extends JFrame implements ActionListener{
     public FoodOrderGUI() {
         setTitle("Food Ordering System");
         orderButton.addActionListener(handler);
-        noneRadioButton.addActionListener(this);
-        a5OffRadioButton.addActionListener(this);
-        a10OffRadioButton.addActionListener(this);
-        a15OffRadioButton.addActionListener(this);
+        rbNone.addActionListener(this);
+        rb5.addActionListener(this);
+        rb10.addActionListener(this);
+        rb15.addActionListener(this);
+        cPizza.addActionListener(this);
+        cFries.addActionListener(this);
+        cBurger.addActionListener(this);
+        cSoftDrinks.addActionListener(this);
+        cTea.addActionListener(this);
+        cSundae.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==noneRadioButton) {
-            disc = 1;
-        } else if (e.getSource()==a5OffRadioButton) {
+        if(e.getSource()== rbNone) {
+            disc = 0;
+        } else if (e.getSource()== rb5) {
             disc = 0.05;
-        } else if (e.getSource()==a10OffRadioButton) {
+        } else if (e.getSource()== rb10) {
             disc = 0.10;
-        } else if (e.getSource()==a15OffRadioButton) {
+        } else if (e.getSource()== rb15) {
             disc = 0.15;
+        } else if (e.getSource()== cBurger) {
+            total = total + 80.00;
+        } else if (e.getSource()== cPizza) {
+            total = total + 100.00;
+        } else if (e.getSource()== cFries) {
+            total = total + 65.00;
+        } else if (e.getSource()== cSoftDrinks) {
+            total = total + 55.00;
+        } else if (e.getSource()== cSundae) {
+            total = total + 40.00;
+        } else if (e.getSource()== cTea) {
+            total = total + 50.00;
         }
     }
+
+
 
     public class orderHandler implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            JOptionPane.showMessageDialog(null,"The total price is Php " + disc);
+            JOptionPane.showMessageDialog(null,"The total price is Php " + (total - (total * disc)) );
+            total = 0;
         }
     }
 
