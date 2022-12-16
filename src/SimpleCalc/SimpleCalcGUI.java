842a2b65fe5cdaf204c06a8b97d7a0497a3e4853
Reyes815 <reyes.rowen30@gmail.com>
2022-12-16 11:44:18 +0800

    renamed all the components in the simple calc package

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,12 +1,41 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SimpleCalcGUI {
+public class SimpleCalcGUI extends JFrame implements ActionListener {
     private JPanel panel1;
     private JTextField tfNumber1;
     private JComboBox cbOperations;
     private JButton btnCompute;
     private JTextField tfNumber;
     private JTextField lblResult;
+    resultHandler result = new resultHandler();
+
+    public static void main(String[] args) {
+        SimpleCalcGUI frame = new SimpleCalcGUI();
+        frame.setContentPane(frame.panel1);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setSize(450,250);
+        frame.setVisible(true);
+    }
+
+    public SimpleCalcGUI(){
+        setTitle("Simple Calculator");
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public class resultHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
 }
