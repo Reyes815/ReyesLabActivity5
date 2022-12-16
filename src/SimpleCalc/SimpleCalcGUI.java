cf6577914ad47d1549f7d91af20971bd94b50368
Reyes815 <reyes.rowen30@gmail.com>
2022-12-16 12:53:04 +0800

    added all the logic into the action listener and completed the program

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class SimpleCalcGUI extends JFrame implements ActionListener {
+public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
     private JTextField tfNumber1;
     private JComboBox cbOperations;
@@ -12,30 +12,55 @@ public class SimpleCalcGUI extends JFrame implements ActionListener {
     private JTextField tfNumber;
     private JTextField lblResult;
     resultHandler result = new resultHandler();
+    int total = 0;
+    String operation;
 
     public static void main(String[] args) {
         SimpleCalcGUI frame = new SimpleCalcGUI();
         frame.setContentPane(frame.panel1);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.setSize(450,250);
+        frame.setSize(600,250);
         frame.setVisible(true);
     }
 
     public SimpleCalcGUI(){
         setTitle("Simple Calculator");
+        btnCompute.addActionListener(result);
 
+        cbOperations.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                operation = (String) cbOperations.getSelectedItem();
+            }
+        });
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
 
     public class resultHandler implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-
+            String inp1 = tfNumber1.getText();
+            String inp2 = tfNumber.getText();
+            try {
+                int num1 = Integer.parseInt(inp1);
+                int num2 = Integer.parseInt(inp2);
+                if (operation.equals("+")){
+                    total = num1 + num2;
+                } else if (operation.equals("-")){
+                    total = num1 - num2;
+                } else if (operation.equals("*")){
+                    total = num1 * num2;
+                } else if (operation.equals("/")){
+                    total = num1 / num2;
+                }
+            } catch (NumberFormatException x) {
+                System.out.println();
+            }
+            String str = total+"";
+            lblResult.setText(str);
+            tfNumber.setText("");
+            tfNumber1.setText("");
         }
     }
 }
